82c377952cf73fbf1460525a5f9c3dbf486c526d
Bogdan-Blaj <blajbogdan94@gmail.com>
2023-03-29 16:47:19 +0300

    DL-18 Changed layout of Storybook

--- src/storiesComponents/Badge/Badge.stories.tsx
+++ src/storiesComponents/Badge/Badge.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, Story } from "@storybook/react";
 import React from "react";
 import { Badge, BadgeProps } from "../../components/Badge/Badge";
 import StoryLayout from "../StoryLayout";
-import { FiStar, FiArrowRight } from "react-icons/fi";
+import { FiStar, FiArrowRight, FiX } from "react-icons/fi";
 import image from "../../Images/NL.svg";
 
 const meta: Meta = {
@@ -30,6 +30,10 @@ const BadgeSizeClasses: Record<BadgeSize, string> = {
 };
 interface Props extends BadgeProps {
   darkMode: boolean;
+  sizeArray?: BadgeSize[];
+  variantArray?: BadgeVariant[];
+  trailingIconArray?: React.ReactElement[];
+  leadingIconArray?: React.ReactElement[];
 }
 
 const StoryBadge: Story<Props> = (args) => (
@@ -61,4 +65,85 @@ Default.args = {
 
 Default.parameters = {
   controls: { exclude: ["LeadingIcon", "TrailingIcon", "className"] },
+};
+
+const StoryBadgeAll: Story<Props> = (args) => {
+  return (
+    <div className="flex flex-col">
+      <StoryLayout {...args} className="grid grid-cols-1 gap-4 ">
+        {args.variantArray?.map((varianColor, index) => (
+          <div className="flex flex-row gap-12">
+            <div className="flex flex-row gap-4">
+              {
+                // badge with no icon
+                args.sizeArray?.map((size, index) => {
+                  return (
+                    <Badge size={size} variant={varianColor}>
+                      Label
+                    </Badge>
+                  );
+                })
+              }
+            </div>
+            <div className="flex flex-row gap-4">
+              {
+                // badge with leading flag icon
+                args.sizeArray?.map((size, index) => {
+                  return (
+                    <Badge
+                      size={size}
+                      variant={varianColor}
+                      LeadingIcon={
+                        <img src={image} alt="nl" className="w-4 h-4" />
+                      }
+                    >
+                      Label
+                    </Badge>
+                  );
+                })
+              }
+            </div>
+            <div className="flex flex-row gap-4">
+              {
+                // badge with trailing X icon
+                args.sizeArray?.map((size, index) => {
+                  return (
+                    <Badge
+                      size={size}
+                      variant={varianColor}
+                      TrailingIcon={<FiX />}
+                    >
+                      Label
+                    </Badge>
+                  );
+                })
+              }
+            </div>
+            <div className="flex flex-row gap-4">
+              {
+                // badge with leading arrow icon
+                args.sizeArray?.map((size, index) => {
+                  return (
+                    <Badge
+                      size={size}
+                      variant={varianColor}
+                      TrailingIcon={<FiArrowRight />}
+                    >
+                      Label
+                    </Badge>
+                  );
+                })
+              }
+            </div>
+          </div>
+        ))}
+      </StoryLayout>
+    </div>
+  );
+};
+export const All = StoryBadgeAll.bind({});
+
+All.args = {
+  sizeArray: ["sm", "md", "lg"],
+  variantArray: ["gray", "primary", "success", "error", "warning"],
 };
